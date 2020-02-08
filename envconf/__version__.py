cb077f8ea7c9288d53d6d0e84100a87db10e2a3e
Sebastien Williams-Wynn <s.williamswynn.mail@gmail.com>
2020-02-08 20:51:21 +0000

    Fix package name

--- envconf/__version__.py
+++ envconf/__version__.py
@@ -2,7 +2,7 @@
 EnvConf ⚙️
 """
 
-__title__ = "envconf"
+__title__ = "py-envconf"
 __description__ = "Managing environment config data"
 __version__ = "0.1.0"
 __dependencies__ = ["toolz", "python-dotenv"]
