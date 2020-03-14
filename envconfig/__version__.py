bd584469a4ae72c3e6eaa2364b64e13e17c3db35
Sebastien Williams-Wynn <s.williamswynn.mail@gmail.com>
2020-03-14 02:17:01 +0000

    Bump version number

--- envconfig/__version__.py
+++ envconfig/__version__.py
@@ -4,5 +4,5 @@ EnvConfig ⚙️
 
 __title__ = "py-envconfig"
 __description__ = "Managing environment config data"
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __dependencies__ = ["toolz", "python-dotenv"]
