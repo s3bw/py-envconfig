89cfe249cabbdf7e7394669754d83faeec39fc67
Sebastien Williams-Wynn <s.williamswynn.mail@gmail.com>
2022-05-19 00:39:31 +0100

    Fix dependencies

--- envconfig/__version__.py
+++ envconfig/__version__.py
@@ -5,9 +5,4 @@ EnvConfig ⚙️
 __title__ = "py-envconfig"
 __description__ = "Managing environment config data"
 __version__ = "0.7.0"
-__dependencies__ = [
-    "toolz",
-    "python-dotenv",
-    # See https://github.com/pypa/setuptools/issues/3293
-    "importlib-metadata>4",
-]
+__dependencies__ = ["toolz", "python-dotenv"]
