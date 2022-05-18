fe2f35fa4a024e813340d3fef04ddf8337f9c292
Sebastien Williams-Wynn <s.williamswynn.mail@gmail.com>
2022-05-19 00:07:43 +0100

    Bump version to 0.7.0

--- envconfig/__version__.py
+++ envconfig/__version__.py
@@ -4,5 +4,5 @@ EnvConfig ⚙️
 
 __title__ = "py-envconfig"
 __description__ = "Managing environment config data"
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 __dependencies__ = ["toolz", "python-dotenv"]
