ed252f41cc3393e41b908d0905604d3fbdeb986a
Sebastien Williams-Wynn <s.williamswynn.mail@gmail.com>
2022-05-19 00:31:16 +0100

    Bump version

--- envconfig/__version__.py
+++ envconfig/__version__.py
@@ -5,4 +5,9 @@ EnvConfig ⚙️
 __title__ = "py-envconfig"
 __description__ = "Managing environment config data"
 __version__ = "0.7.0"
-__dependencies__ = ["toolz", "python-dotenv"]
+__dependencies__ = [
+    "toolz",
+    "python-dotenv",
+    # See https://github.com/pypa/setuptools/issues/3293
+    "importlib-metadata>4",
+]
